c1d5e25e35cdb1d54ab5bb0c5ce5f4dc9ecdc02d
brgndy <brgndy96@gmail.com>
2023-02-18 22:01:23 +0900

    background fixed

---
+++ buildcarousel/src/App.tsx
@@ -0,0 +1,30 @@
+import img1 from "./assets/1.png";
+import img2 from "./assets/2.png";
+import img3 from "./assets/3.png";
+import classes from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <div className={classes.container}>
+        {/* <img
+          className={`${classes.card} ${classes.card_active}`}
+          src={img1}
+          alt="chordImg"
+        ></img>
+        <img
+          className={`${classes.card} ${classes.card_left}`}
+          src={img2}
+          alt="chordImg"
+        ></img>
+        <img
+          className={`${classes.card} ${classes.card_right}`}
+          src={img3}
+          alt="chordImg"
+        ></img> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
