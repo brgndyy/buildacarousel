99b2cf08a2184b5cfae3c5054b0fb79ce8f4366a
brgndy <brgndy96@gmail.com>
2023-02-19 00:23:34 +0900

    scroll

--- buildcarousel/src/App.tsx
+++ buildcarousel/src/App.tsx
@@ -6,23 +6,21 @@ import classes from "./App.module.css";
 function App() {
   return (
     <>
-      <div className={classes.container}>
-        {/* <img
-          className={`${classes.card} ${classes.card_active}`}
-          src={img1}
-          alt="chordImg"
-        ></img>
-        <img
-          className={`${classes.card} ${classes.card_left}`}
-          src={img2}
-          alt="chordImg"
-        ></img>
-        <img
-          className={`${classes.card} ${classes.card_right}`}
-          src={img3}
-          alt="chordImg"
-        ></img> */}
-      </div>
+      <img
+        className={`${classes.card} ${classes.card_active}`}
+        src={img1}
+        alt="chordImg"
+      ></img>
+      <img
+        className={`${classes.card} ${classes.card_left}`}
+        src={img2}
+        alt="chordImg"
+      ></img>
+      <img
+        className={`${classes.card} ${classes.card_right}`}
+        src={img3}
+        alt="chordImg"
+      ></img>
     </>
   );
 }
