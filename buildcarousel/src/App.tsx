f93944e5807ae90c6fc0548c0a77be5b7ec62a59
brgndy <brgndy96@gmail.com>
2023-02-19 11:01:39 +0900

    mag

--- buildcarousel/src/App.tsx
+++ buildcarousel/src/App.tsx
@@ -2,25 +2,60 @@ import img1 from "./assets/1.png";
 import img2 from "./assets/2.png";
 import img3 from "./assets/3.png";
 import classes from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const cards = [
+    { id: "1", image: img1 },
+    { id: "2", image: img2 },
+    { id: "3", image: img3 },
+  ];
+
+  const mod = (n: any, m: any) => {
+    let result = n % m;
+
+    //Return a postive value
+
+    return result >= 0 ? result : result + m;
+  };
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      setIndex((index + 1) % cards.length);
+    }, 3000);
+    return () => clearTimeout(interval);
+  }, [index, cards.length]);
+
   return (
     <>
-      <img
-        className={`${classes.card} ${classes.card_active}`}
-        src={img1}
-        alt="chordImg"
-      ></img>
-      <img
-        className={`${classes.card} ${classes.card_left}`}
-        src={img2}
-        alt="chordImg"
-      ></img>
-      <img
-        className={`${classes.card} ${classes.card_right}`}
-        src={img3}
-        alt="chordImg"
-      ></img>
+      <div className={classes.carousel}>
+        {cards.map((item, i) => {
+          const indexLeft = mod(index - 1, cards.length);
+          const indexRight = mod(index + 1, cards.length);
+          let className = "";
+
+          if (i === index) {
+            className = `${classes.card} ${classes.card_active}`;
+          } else if (i === indexRight) {
+            className = `${classes.card} ${classes.card_right}`;
+          } else if (i === indexLeft) {
+            className = `${classes.card} ${classes.card_left}`;
+          } else {
+            className = `${classes.card}`;
+          }
+
+          return (
+            <img
+              key={item.id}
+              className={className}
+              src={item.image}
+              alt="chordImg"
+            ></img>
+          );
+        })}
+      </div>
     </>
   );
 }
